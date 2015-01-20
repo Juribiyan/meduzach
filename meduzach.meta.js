1f3a263c3a8f0f6d5f84c54a59c605c7738a6599
Juribiyan <snivy@ponyasha.ru>
2015-01-20 16:46:00 +0600

    Create meduzach.meta.js

---
+++ meduzach.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Meduzach
+// @version      0.1
+// @updateURL    https://raw.github.com/Juribiyan/meduzach/master/meduzach.meta.js
+// @description  Disqus comments for Meduza.io
+// @author       Juribiyan
+// @match        meduza.io/*
+// @grant        none
+// ==/UserScript==
