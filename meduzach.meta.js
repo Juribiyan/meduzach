a44c1cf7c121947f426e469b1c11f87aff0f2495
Juribiyan <snivy@ponyasha.ru>
2015-01-21 13:16:53 +0600

    Update meduzach.meta.js

--- meduzach.meta.js
+++ meduzach.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Meduzach
-// @version      0.1
+// @version      0.2
 // @updateURL    https://raw.github.com/Juribiyan/meduzach/master/meduzach.meta.js
 // @description  Disqus comments for Meduza.io
 // @author       Juribiyan
